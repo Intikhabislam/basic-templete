00af98b3407db30a9467eabfec32fe00c2c38786
Intikhab <islaminitkhab2gmail.com>
2023-05-16 17:52:07 +0530

    First-commit-done

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
                   >
                    <hr />
                 </Section>
-                    )
+                     )
 
                 }
                 )
